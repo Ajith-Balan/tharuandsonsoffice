8a3504aa55bc3b5ca9715b1e8f8d2474edd0934f
AJITH B <139620991+Ajith-Balan@users.noreply.github.com>
2025-11-27 15:00:40 +0530

    Update AllBills.jsx

--- src/pages/Admin/AllBills.jsx
+++ src/pages/Admin/AllBills.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react'
-import Layout from '../../components/Layout/Layout'
+import Layout from '../../components/layout/Layout'
 import Adminpanel from '../../components/layout/Adminpanel'
 import axios from 'axios'
 import { useAuth } from '../../context/Auth'
