2c9116068af8ec6c732062dd83956cd2045bee2e
AJITH B <139620991+Ajith-Balan@users.noreply.github.com>
2025-11-27 14:56:37 +0530

    Update Dashboard.jsx

--- src/pages/Admin/Dashboard.jsx
+++ src/pages/Admin/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Layout from "../../components/Layout/Layout";
+import Layout from "../../components/layout/Layout";
 import {
   FaFileInvoiceDollar,
   FaExclamationTriangle,
