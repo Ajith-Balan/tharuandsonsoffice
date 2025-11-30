1680fb8c0c242b6b1ba3e1b91657b7b8500f7af7
Ajith-Balan <ajithaji9404@gmail.com>
2025-12-01 01:19:11 +0530

    added

--- src/pages/Admin/AllBills.jsx
+++ src/pages/Admin/AllBills.jsx
@@ -1,12 +1,10 @@
 
 import React, { useState, useEffect } from "react";
-import Layout from "../../components/Layout/Layout";
 import Adminpanel from "../../components/layout/Adminpanel";
 import axios from "axios";
 import { useAuth } from "../../context/Auth";
 
 
-import React,{useState,useEffect} from 'react'
 import Layout from '../../components/layout/Layout'
 import Adminpanel from '../../components/layout/Adminpanel'
 import axios from 'axios'
