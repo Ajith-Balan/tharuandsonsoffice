b3dd7e3931b7981856725ede7cfea7c7e6221f0b
Ajith-Balan <ajithaji9404@gmail.com>
2025-12-05 19:30:55 +0530

    added

--- src/pages/Admin/Ers/Pages/Completedtrain.jsx
+++ src/pages/Admin/Ers/Pages/Completedtrain.jsx
@@ -249,7 +249,7 @@ const Completedtrain = () => {
 
                               <td className="px-4 py-3 border-b">
                                 <Link
-                                  to={`/dashboard/manager/traindetail/${train._id}`}
+                                  to={`/dashboard/admin/ers/traindetails/${train._id}`}
                                   className="text-blue-600 hover:underline flex items-center gap-2"
                                 >
                                   Details <FaEdit />
